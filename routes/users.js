152c9999c7b38180c698318f279dc70ff038d4a4
Rehaan Mazid <rehaanmazid@gmail.com>
2021-05-15 23:53:10 +0530

    feat: project complete

---
+++ routes/users.js
@@ -0,0 +1,407 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const verify = require("./verifyToken");
+const { Auth } = require("two-step-auth");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+const Otp = require("../models/Otp");
+
+// @TODO Add recaptcha middleware
+router.post("/login", (req, res) => {
+  // CHECKING IF EMAIL EXISTS
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(400).json({ errorMessage: "Email Does Not Exist" });
+      }
+
+      // CHECKING IF PASSWORD IS CORRECT
+        bcrypt.compare(req.body.password, user.password).then(function(result) {
+            if(!result)
+            {
+                return res.status(400).json({ errorMessage: "Invalid Credentials" });
+            }
+            else
+            {
+                  // CREATE AND ASSIGN A TOKEN
+                    const token = jwt.sign(
+                        {
+                        _id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        phoneNo: user.phoneNo,
+                        },
+                        process.env.TOKEN_SECRET
+                    );
+                    res.status(200).json({
+                        success: true,
+                        message: "Authentication Successful!",
+                        token: token,
+                    });
+            }
+         });
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      res.status(400).json({
+        errorText: err,
+      });
+    });
+});
+
+router.get("/dashboard", verify, (req, res) => {
+  return res.status(400).json({
+    message: req.user.name + " Logged In",
+  });
+});
+
+router.get("/test", (req, res) => {
+  const current = new Date();
+  let hours = current.getHours();
+  body = current.getMinutes().toString();
+  if (body.length == 1) body = "0" + body;
+  return res.status(200).json({
+    message: body,
+  });
+});
+
+
+router.get("/getAll", verify, (req, res) => {
+  if (req.user._id.equals(process.env.ADMIN)) {
+    User.find().then((infos) => {
+      res.status(200).json(infos);
+    });
+  } else {
+    return res.status(400).json({
+      errorMessage: "unauthorized access request",
+    });
+  }
+});
+
+
+router.get("/profile", verify, (req, res) => {
+  User.findOne({ email: req.user.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(400).json({
+          Message: "User doesn't Exists!",
+        });
+      }
+      return res.status(200).json({
+        name: user.name,
+        email: user.email,
+        phoneNo: user.phoneNo,
+      });
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      return res.status(400).json({
+        Error: err,
+      });
+    });
+});
+
+router.post("/forgotPassword", (req, res) => {
+  if (!req.body.email) {
+    return res.status(400).json({
+      errorMessage: "Missing Required Params",
+    });
+  }
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(400).json({
+          errorMessage: "User Doesnt Exists",
+        });
+      } else {
+        login(req.body.email, "forgotPassword");
+        return res.status(200).json({
+          otpSentStatus: "success",
+          message: "call updatePassowrd otp verification endpoint",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+});
+
+router.patch("/updatePassword", (req, res) => {
+  if (!req.body.password || !req.body.otp || !req.body.email) {
+    return res.status(400).json({
+      errorMessage: "Missing Required Params",
+    });
+  }
+
+  Otp.findOne({ otp: req.body.otp })
+    .then((otp) => {
+      if (!otp) {
+        return res.status(400).json({
+          errorMessage: "OTP does not exist",
+        });
+      } else {
+        const current = new Date();
+        const hours = current.getHours();
+        var expiryMinutes = current.getMinutes().toString();
+        if (expiryMinutes.length == 1) expiryMinutes = "0" + expiryMinutes;
+        const expiryTime = hours.toString() + ":" + expiryMinutes.toString();
+        if (
+          otp.email !== req.body.email ||
+          otp.requirement !== "forgotPassword"
+        ) {
+          return res.status(400).json({
+            errorMessage: "OTP Not Valid",
+          });
+        }
+        if (expiryTime.localeCompare(otp.expiryTime) > 0) {
+          return res.status(400).json({
+            errorMessage: "OTP expired",
+          });
+        } else {
+          User.findOne({ email: req.body.email })
+            .then((user) => {
+              if (!user) {
+                return res.status(400).json({
+                  errorMessage: "User doesn't Exists!",
+                });
+              } else {
+                // hash
+                bcrypt.genSalt(10, (err, salt) => {
+                  if (err) {
+                    return res.status(400).json({
+                      errorMessage: err,
+                    });
+                  }
+                  bcrypt.hash(req.body.password, salt, (err, hash) => {
+                    if (err) {
+                      return res.status(400).json({
+                        errorMessage: err,
+                      });
+                    }
+
+                    User.updateOne(
+                      { email: req.body.email },
+                      {
+                        $set: {
+                          password: hash,
+                        },
+                      }
+                    )
+                      .then((update) => {
+                        Otp.deleteOne({ _id: otp._id })
+                          .then(() => {
+                            res.status(200).json({
+                              message: "Details updated Successfully!",
+                            });
+                          })
+                          .catch((err) => {
+                            console.log("Error:", err);
+                          });
+                      })
+                      .catch((err) => {
+                        console.log("Error:", err);
+                      });
+                  });
+                });
+              }
+            })
+            .catch((err) => {
+              console.log("Error:", err);
+            });
+        }
+      }
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+});
+
+router.get("/logout", (req, res) => {
+  return res.status(200).json({
+    message: "logged out",
+  });
+});
+
+let otp = 0;
+
+async function login(emailId, requirement) {
+  try {
+    const res = await Auth(emailId, "HPAIR");
+    otp = res.OTP;
+    const current = new Date();
+    let hours = current.getHours();
+    console.log(current.getMinutes().toString());
+    let expiryMinutes = current.getMinutes() + 10;
+    if (expiryMinutes > 60) {
+      hours = hours + 1;
+      if (hours == 24) {
+        hours = "00";
+      }
+      expiryMinutes = expiryMinutes - 60;
+    }
+    Otp.deleteMany({ email: emailId })
+      .then(() => {
+        var email = emailId;
+        const expiryTime = hours.toString() + ":" + expiryMinutes.toString();
+        const newOtp = new Otp({
+          otp,
+          email,
+          expiryTime,
+          requirement,
+        });
+        newOtp
+          .save()
+          .then((otp) => {
+            console.log("done");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(res.success);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+router.post("/otpVerify", (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.password ||
+    !req.body.phoneNo ||
+    !req.body.otp
+  ) {
+    return res.status(400).json({
+      errorMessage: "Missing Required Params",
+    });
+  }
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({
+          errorMessage: "User Already Exists",
+        });
+      } else {
+        Otp.findOne({ otp: req.body.otp })
+          .then((otp) => {
+            if (!otp) {
+              return res.status(400).json({
+                errorMessage: "OTP does not exist",
+              });
+            } else {
+              const current = new Date();
+              const hours = current.getHours();
+              var expiryMinutes = current.getMinutes().toString();
+              if (expiryMinutes.length == 1)
+                expiryMinutes = "0" + expiryMinutes;
+              console.log(expiryMinutes.toString());
+              const expiryTime =
+                hours.toString() + ":" + expiryMinutes.toString();
+              if (
+                otp.email !== req.body.email ||
+                otp.requirement !== "registration"
+              ) {
+                return res.status(400).json({
+                  errorMessage: "OTP Not Valid",
+                });
+              }
+              if (expiryTime.localeCompare(otp.expiryTime) > 0) {
+                return res.status(400).json({
+                  errorMessage: "OTP expired",
+                });
+              } else {
+                const name = req.body.name;
+                const email = req.body.email;
+                const phoneNo = req.body.phoneNo;
+                const password = req.body.password;
+
+                const newUser = new User({
+                  name,
+                  password,
+                  email,
+                  phoneNo,
+                });
+
+                // hash
+                bcrypt.genSalt(10, (err, salt) => {
+                  if (err) {
+                    return res.status(400).json({
+                      errorMessage: err,
+                    });
+                  }
+                  bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) {
+                      return res.status(400).json({
+                        errorMessage: err,
+                      });
+                    }
+
+                    newUser.password = hash;
+                    newUser
+                      .save()
+                      .then((user) => {
+                        Otp.deleteOne({ _id: otp._id })
+                          .then(() => {
+                            return res.status(200).json({
+                              message: "success",
+                            });
+                          })
+                          .catch((err) => {
+                            return res.status(400).json({
+                              errorMessage: err,
+                            });
+                          });
+                      })
+                      .catch((err) => {
+                        return res.status(400).json({
+                          errorMessage: err,
+                        });
+                      });
+                  });
+                });
+              }
+            }
+          })
+          .catch((err) => {
+            console.log("Error:", err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+});
+
+router.post("/register", (req, res) => {
+  if (!req.body.email) {
+    return res.status(400).json({
+      errorMessage: "Missing Required Params",
+    });
+  }
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({
+          errorMessage: "User Already Exists",
+        });
+      } else {
+        login(req.body.email, "registration");
+        return res.status(200).json({
+          otpSentStatus: "success",
+          message: "call otp verification endpoint",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+});
+
+module.exports = router;
